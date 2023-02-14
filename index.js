a2817613d9437e243194591dec68611361c2757a
aurore <rigault.aurore@gmail.com>
2023-02-14 11:19:39 +0100

    message d'acceuil et bouton pour supprimer la div d'acceuil

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ text.textContent=`toto`;
 div.appendChild(text);
 let main= document.getElementById("main");
 main.appendChild(div);
+
+var today = new Date();
+var hourNow = today.getHours();
+var greetings;
+
+if (hourNow < 12) {
+    greetings = "Good Morning!";
+}
+else if (hourNow < 20) {
+    greetings = "Good Afternoon!";
+}
+else if (hourNow < 24) {
+    greetings = "Good evening!";
+}
+else {
+    greetings = "Welcome!";
+}
+
+document.getElementById("col-1").innerHTML = "<h3>" + greetings + "</h3>";
+
+document.getElementById("button-del").onclick = function(){
+    document.getElementById('top-section').innerHTML = "";
+}
